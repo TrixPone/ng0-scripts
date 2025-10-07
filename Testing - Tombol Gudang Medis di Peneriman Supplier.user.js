643f20c64346f2d8a712cf3598cd0f67a880a05c
TrixPone <35420947+TrixPone@users.noreply.github.com>
2025-10-07 12:21:05 +0800

    Update Testing - Tombol Gudang Medis di Peneriman Supplier.user.js

--- Testing - Tombol Gudang Medis di Peneriman Supplier.user.js
+++ Testing - Tombol Gudang Medis di Peneriman Supplier.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Testing - Tombol Gudang Medis di Peneriman Supplier
-// @namespace    http://tampermonkey.net/
-// @version      2025-07-24
-// @description  try to take over the world!
-// @author       You
+// @namespace    http://rsupkandou.com
+// @version      2025-10-24
+// @description  Automatically set the date to 1st of 2025 and options to Gudang Medis with a single click
+// @author       TrixPone
 // @match        https://ng0.rsupkandou.com:3000/laporan-farmasi/get-penerimaan-supplier
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=rsupkandou.com
 // @grant        none
+// @updateURL    https://github.com/TrixPone/ng0-scripts/raw/refs/heads/main/Testing%20-%20Tombol%20Gudang%20Medis%20di%20Peneriman%20Supplier.user.js
 // ==/UserScript==
 
 (function() {
