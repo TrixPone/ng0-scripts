697e175b288328b3cc9a74efa3ed35903f66b453
TrixPone <35420947+TrixPone@users.noreply.github.com>
2026-02-09 22:03:00 +0800

    Update Testing - Tombol History Apol di Nomor Peserta.user.js

--- Testing - Tombol History Apol di Nomor Peserta.user.js
+++ Testing - Tombol History Apol di Nomor Peserta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Testing - Tombol History Apol di Nomor Peserta
 // @namespace    http://rsupkandou.com
-// @version      2026-01-02
+// @version      2026-02-09
 // @description  Testing Add Tombol History Apol di SPA Farmasi
 // @author       TrixPone
 // @match        */spa-farmasi
@@ -19,85 +19,67 @@
 /* globals jQuery, $, waitForKeyElements */
 
 function addBTNApol () {
-    'use strict';
+  'use strict';
 
-    // define variables
-var NoPeserta = document.getElementsByClassName('col-12 mt-1')[2].innerText;
-var URLHistoryApol = 'https://apotek.bpjs-kesehatan.go.id/apotek/frmHistObat.aspx'
+  const pesertaValueEl = document.getElementsByClassName('col-12 mt-1')[2];
+  const pesertaLabelEl = document.getElementsByClassName('col-12 mt-2')[3];
 
-var GrayBox = document.getElementsByClassName('col-12 mt-2')[3];
-var button = document.createElement("Button");
-var NumberApol = NoPeserta
+  if (!pesertaValueEl || !pesertaLabelEl) return;
 
-// define button properties
-button.innerHTML = "Buka History Apol";
-button.onclick = openApol;
-button.className = 'btn btn-sm btn-success text-light get-print'
+  // 🚫 STOP if button already exists in this container
+  if (pesertaLabelEl.querySelector('.tm-btn-apol')) return;
 
-    //append the button
-GrayBox.appendChild(button);
-    function openApol() {
-        //using GM values to make it persists between SPA and Apol
-        GM.setValue ("NumberApol", NoPeserta)
-        window.open(URLHistoryApol);
-        console.log(GM.listValues());
-    }
+  const NoPeserta = pesertaValueEl.innerText.trim();
+  const URLHistoryApol = 'https://apotek.bpjs-kesehatan.go.id/apotek/frmHistObat.aspx';
 
-//add new copy function
-var copyButton = document.createElement("icon");
-copyButton.className = 'fa fa-copy'
+  // === Button ===
+  const button = document.createElement('button');
+  button.innerHTML = 'Buka History Apol';
+  button.className = 'btn btn-sm btn-success text-light get-print tm-btn-apol';
+  button.onclick = function () {
+    GM.setValue('NumberApol', NoPeserta);
+    window.open(URLHistoryApol);
+  };
 
-document.getElementsByClassName('col-12 mt-1')[2].appendChild(copyButton);
+  pesertaLabelEl.appendChild(button);
 
-copyButton.addEventListener("click", function (e) {
-if (e.target.dataset.added) return;
+  // === Copy Icon ===
+  if (!pesertaValueEl.querySelector('.tm-copy-apol')) {
+    const copyButton = document.createElement('icon');
+    copyButton.className = 'fa fa-copy tm-copy-apol';
+    copyButton.style.cursor = 'pointer';
 
-     navigator.clipboard.writeText(document.getElementsByClassName('col-12 mt-1')[2].innerText)
+    copyButton.addEventListener('click', function () {
+      navigator.clipboard.writeText(NoPeserta)
         .then(() => {
-          // feedback text
           const msg = document.createTextNode(' Copied!');
-          e.target.after(msg);
-
+          copyButton.after(msg);
           setTimeout(() => msg.remove(), 2000);
         })
-        .catch(err => {
-          console.error('Copy failed', err);
-        });
-});
+        .catch(err => console.error('Copy failed', err));
+    });
+
+    pesertaValueEl.appendChild(copyButton);
+  }
 }
 
-
-
-//wait for SPA element to show up first
-waitForKeyElements (
-    "div.col-12.mt-3",
-    addBTNApol, false
+// Wait for SPA sidebar rebuilds
+waitForKeyElements(
+  'div.col-12.mt-3',
+  addBTNApol,
+  false
 );
 
-
-//unused Apol function, backed up just in case
-/*
-waitForKeyElements (
-    "div#ctl00_ctl00_ASPxSplitter1_Content_ContentSplitter_1_CC",
-    ApolFunction, false
-);
-*/
-
-//need to async, since GM values isn't instantly retrieved
+// === APOL PAGE AUTO FILL ===
 (async function ApolFunction () {
-    var NumberApol = await GM.getValue("NumberApol")
-    console.log(await GM.getValue("NumberApol"));
-    const currentURLPath = window.location.pathname
-    if (NumberApol === undefined){
-//        GM.notification("No Number", "No Data")
-        console.log(await GM.getValue("NumberApol"));
-    } else if (currentURLPath !== '/spa-farmasi') {
-//        GM.notification("Pasted data from SPA", "Value Get!");
-        //hopefully these elements doesn't change over time
-        document.getElementById('ctl00_ctl00_ASPxSplitter1_Content_ContentSplitter_MainContent_txtNOKAPST_I').value = NumberApol
-        document.getElementById('ctl00_ctl00_ASPxSplitter1_Content_ContentSplitter_MainContent_BtnCari_CD').click();
-        //delete the value after using it
-        GM.deleteValue("NumberApol")
-    }
-}
-)();
+  const NumberApol = await GM.getValue('NumberApol');
+  const currentURLPath = window.location.pathname;
+
+  if (!NumberApol) return;
+
+  if (currentURLPath !== '/spa-farmasi') {
+    document.getElementById('ctl00_ctl00_ASPxSplitter1_Content_ContentSplitter_MainContent_txtNOKAPST_I').value = NumberApol;
+    document.getElementById('ctl00_ctl00_ASPxSplitter1_Content_ContentSplitter_MainContent_BtnCari_CD').click();
+    GM.deleteValue('NumberApol');
+  }
+})();
